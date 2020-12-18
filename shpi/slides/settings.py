44dc77ddb84c9ce1ca48418c216811ec58ebc7bc
SHPI <46942752+shpi@users.noreply.github.com>
2020-12-18 13:34:26 +0100

    Update settings.py

--- shpi/slides/settings.py
+++ shpi/slides/settings.py
@@ -46,9 +46,7 @@ newtxt = pi3d.TextBlock(-300, -180, 0.1, 0.0, 15, text_format=unichr(0xE001),
                 size=0.99, spacing="F", space=0.05, colour=(1.0, 1.0, 1.0, 1.0))
 text2.add_text_block(newtxt)
 
-update = pi3d.TextBlock(-255, -225, 0.1, 90.0, 15, text_format=unichr(0xE034),
-                size=0.79, spacing="F", space=0.05, colour=(1.0, 1.0, 1.0, 1.0))
-text2.add_text_block(update)
+
 
 textblock = pi3d.TextBlock(-270, 0, 0.1, 0.0, 170, data_obj=peripherals.eg_object,
                 attr="ssid", text_format="SSID: {:s}", size=0.29, spacing="F",
@@ -86,14 +84,6 @@ def inloop(textchange=False, activity=False, offset=0):
             text2.regen()
         elif peripherals.clicked(wifi.x, wifi.y):
             config.subslide = 'wifisetup'
-        elif peripherals.clicked(update.x, update.y):
-            import subprocess
-            process = subprocess.Popen(['git', 'pull', '-v', 'origin', 'master'],
-                        cwd='/home/pi/zero_main_application', stdout=subprocess.PIPE)
-            logging.info(process.communicate()[0])
-            process2 = subprocess.Popen(['git', 'pull', '-v', 'origin', 'master'],
-                        cwd='/home/pi/zero_avr_firmware_std', stdout=subprocess.PIPE)
-            logging.info(process2.communicate()[0])
 
     if offset != 0:
         offset = graphics.slider_change(text2.text, offset)
